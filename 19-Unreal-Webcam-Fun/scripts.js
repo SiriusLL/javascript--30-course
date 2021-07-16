c5a9244df4bba170fc9a7c8b9d35e506c8b2d036
SiriusLL <sirius.love.light@gmail.com>
2021-07-15 23:21:33 -0600

    redEffect

--- 19-Unreal-Webcam-Fun/scripts.js
+++ 19-Unreal-Webcam-Fun/scripts.js
@@ -27,6 +27,14 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    // take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // console.log(pixels); //careful
+    // mess with them
+    pixels = redEffect(pixels);
+    // debugger;
+    // put them back
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
@@ -44,6 +52,15 @@ function takePhoto() {
   // link.textContent = "Download Image";
   link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
   strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.lenght; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; //red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; //green
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //blue
+  }
+  return pixels;
 }
 
 getVideo();
