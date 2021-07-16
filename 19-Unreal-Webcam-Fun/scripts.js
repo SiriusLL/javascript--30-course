4be64b54e5b3dd37b28dac5c9390793051e4f531
SiriusLL <sirius.love.light@gmail.com>
2021-07-15 23:44:18 -0600

    rgbSplit fn

--- 19-Unreal-Webcam-Fun/scripts.js
+++ 19-Unreal-Webcam-Fun/scripts.js
@@ -29,10 +29,11 @@ function paintToCanvas() {
     ctx.drawImage(video, 0, 0, width, height);
     // take the pixels out
     let pixels = ctx.getImageData(0, 0, width, height);
-    // console.log(pixels); //careful
+
     // mess with them
-    pixels = redEffect(pixels);
-    // debugger;
+    // pixels = redEffect(pixels);
+    pixels = rgbSplit(pixels);
+    ctx.globalAlpha = 0.1;
     // put them back
     ctx.putImageData(pixels, 0, 0);
   }, 16);
@@ -55,10 +56,19 @@ function takePhoto() {
 }
 
 function redEffect(pixels) {
-  for (let i = 0; i < pixels.data.lenght; i += 4) {
-    pixels.data[i + 0] = pixels.data[i + 0] + 100; //red
-    pixels.data[i + 1] = pixels.data[i + 1] - 50; //green
-    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //blue
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0] + 200; // RED
+    pixels.data[i + 100] = pixels.data[i + 1] - 50; // GREEN
+    pixels.data[i - 150] = pixels.data[i + 2] * 0.5; // Blue
   }
   return pixels;
 }
