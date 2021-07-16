8eb25cf99f21479d4818499b17761b5fb35fcac6
SiriusLL <sirius.love.light@gmail.com>
2021-07-15 22:59:54 -0600

    take photo fn

--- 19-Unreal-Webcam-Fun/scripts.js
+++ 19-Unreal-Webcam-Fun/scripts.js
@@ -30,4 +30,22 @@ function paintToCanvas() {
   }, 16);
 }
 
+function takePhoto() {
+  //played the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  //take the data out of the canvas
+  const data = canvas.toDataURL("image/jpeg");
+  // console.log(data);
+  const link = document.createElement("a");
+  link.href = data;
+  link.setAttribute("download", "handsome");
+  // link.textContent = "Download Image";
+  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
 getVideo();
+
+video.addEventListener("canplay", paintToCanvas);
