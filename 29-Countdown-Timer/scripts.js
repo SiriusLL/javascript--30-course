f43b040888df03d3b7462970c281b9b9a6d154d9
SiriusLL <sirius.love.light@gmail.com>
2021-07-27 23:51:12 -0600

    run function immediatly first time before interval

--- 29-Countdown-Timer/scripts.js
+++ 29-Countdown-Timer/scripts.js
@@ -3,13 +3,20 @@ let countdown;
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
-  console.log(now, then);
+  // console.log(now, then);
+  displayTimeLeft(seconds);
+
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
     // check if we should stop
     if (secondsLeft <= 0) {
       clearInterval(countdown);
     }
-    console.log(secondsLeft);
+    //display it
+    displayTimeLeft(secondsLeft);
   }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  console.log(seconds);
 }
